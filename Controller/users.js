00a26987e5bd9b75d693542a573b2e043c3c7fcb
ERuvinga <ruvingaelie@gmail.com>
2023-02-15 12:07:14 +0200

    begin (login) endpoint

--- Controller/users.js
+++ Controller/users.js
@@ -1,8 +1,23 @@
 // all controller for users
+const modelSchema = require('../Models/user');
 
 //controller that control a login endpoint
 exports.login = (req, res) => {
     console.log(req.body);
+
+    user = new modelSchema({
+        ...req.body
+    }); // create a new user and check it with model and shema
+
+    // if this data correct, we save it
+    user.Save()
+        .then(() => {
+            console.log('userCreate success');
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
 }
 
 // controller that control a register endpoint
