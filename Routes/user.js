0266fb1b2b0cbedc48c42efba64f6ed90981b713
ERuvinga <ruvingaelie@gmail.com>
2023-02-15 11:55:08 +0200

    implement and fix register and login routes

--- Routes/user.js
+++ Routes/user.js
@@ -1,13 +1,9 @@
 
 const express = require("express");
 const router = express.Router();
+const UsersCtrl = require('../Controller/users');
 
-router.post = () => {
-
-};
-
-router.post = () => {
-
-}
+router.post('/login', UsersCtrl.login);
+router.post('/register', UsersCtrl.register);
 
 module.exports = router; 
