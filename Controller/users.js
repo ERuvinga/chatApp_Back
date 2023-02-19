c1069b8fbdc09a92a855a8a8a05cf9b9d023116a
ERuvinga <ruvingaelie@gmail.com>
2023-02-19 02:12:50 +0200

    adding bcrypt to hash a password and save it in data base

--- Controller/users.js
+++ Controller/users.js
@@ -1,5 +1,6 @@
 // all controller for users
 const modelSchema = require('../Models/user');
+const bcrypt = require('bcrypt');
 
 //controller that control a login endpoint
 exports.login = (req, res) => {
@@ -10,21 +11,35 @@ exports.login = (req, res) => {
 
 // controller that control a register endpoint
 exports.register = (req, res) => {
-    console.log(req.body);
-    user = new modelSchema({
-        ...req.body
-    }); // create a new user and check it with model and shema
 
-    // if this data correct, we save it
-    user.save()
-        .then((dataUser) => {
-            console.log(`${dataUser}user Create success`);
-            res.status(200);
-            res.json({ dataUser });
+    console.log(req.body);
+    const saltCrypt = 10;
+
+    bcrypt.hash(req.body.password, saltCrypt)
+        .then(passwordCrypt => {
+
+            user = new modelSchema({
+                email: req.body.email,
+                password: passwordCrypt
+            }); // create a new user and check it with model and shema
+
+            // if this data correct, we save it
+            user.save()
+                .then((dataUser) => {
+                    console.log(`${dataUser}user Create success`);
+                    res.status(200);
+                    res.json({ dataUser });
+                })
+                .catch(error => {
+                    res.status(400);
+                    res.json({ error });
+                    console.error(error);
+                });
         })
+
         .catch(error => {
-            res.status(401);
+            res.status(500);
             res.json({ error });
             console.error(error);
-        });
+        })
 }
