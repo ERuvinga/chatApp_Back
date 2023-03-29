a35613db50581ad3d6dd52731ac5c105152930cd
ERuvinga <ruvingaelie@gmail.com>
2023-03-29 11:03:54 +0200

    reorganisation routes

--- Routes/conversation.js
+++ Routes/conversation.js
@@ -5,9 +5,6 @@ const Auth = require('../Middleware/Auth');
 
 //Conversations Controllers
 router.post('/', Auth, ConversationCtrl.NewConversation);
-router.put('/NewMessage/:id', Auth, ConversationCtrl.AddNewMessage);
-
-router.post('/newConversation', Auth, ConversationCtrl.NewConversation);
-router.post('/newMessage', Auth, ConversationCtrl.AddNewMessage);
+router.put('/NewMessage/:idConversat', Auth, ConversationCtrl.AddNewMessage);
 
 module.exports = router;
